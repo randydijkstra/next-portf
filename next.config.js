2b57f3d5fade95a6752f37ed3344933a9d6e28fa
Randy <randydijkstra92@gmail.com>
2022-10-29 18:46:18 +0200

    ci: some basic deployment tryouty

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const prod = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: prod ? '/next-portf/' : '',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
